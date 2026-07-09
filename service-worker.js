5d41a86415ee1532871fbd9b525837f5ed593fb2
MD. Musaddek Hussain (MUSA) <mdmusaddekhussain13@gmail.com>
2026-07-09 23:36:36 +0600

    Add service worker for file caching
    Implement service worker for caching files.

---
+++ service-worker.js
@@ -0,0 +1,53 @@
+const cacheName="snake-musa-v1";
+
+
+const files=[
+"index.html",
+"style.css",
+"script.js",
+"manifest.json",
+"snake.png"
+];
+
+
+
+self.addEventListener(
+"install",
+event=>{
+
+event.waitUntil(
+
+caches.open(cacheName)
+.then(cache=>{
+
+return cache.addAll(files);
+
+})
+
+);
+
+});
+
+
+
+
+
+self.addEventListener(
+"fetch",
+event=>{
+
+
+event.respondWith(
+
+caches.match(event.request)
+
+.then(response=>{
+
+return response || fetch(event.request);
+
+})
+
+);
+
+
+});
