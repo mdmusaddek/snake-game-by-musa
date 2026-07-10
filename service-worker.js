9880923dd94c0ff122686c7b5830e8d45f0a7bc2
MD. Musaddek Hussain (MUSA) <mdmusaddekhussain13@gmail.com>
2026-07-10 21:29:11 +0600

    Refactor service worker for caching and fetch handling

--- service-worker.js
+++ service-worker.js
@@ -1,53 +1,27 @@
-const cacheName="snake-musa-v1";
+const cacheName = "snake-game-by-musa-v1";
 
-
-const files=[
-"index.html",
-"style.css",
-"script.js",
-"manifest.json",
-"snake.png"
+const filesToCache = [
+    "index.html",
+    "style.css",
+    "manifest.json",
+    "snake.png"
 ];
 
-
-
-self.addEventListener(
-"install",
-event=>{
-
-event.waitUntil(
-
-caches.open(cacheName)
-.then(cache=>{
-
-return cache.addAll(files);
-
-})
-
-);
-
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(cacheName)
+        .then(cache => {
+            return cache.addAll(filesToCache);
+        })
+    );
 });
 
 
-
-
-
-self.addEventListener(
-"fetch",
-event=>{
-
-
-event.respondWith(
-
-caches.match(event.request)
-
-.then(response=>{
-
-return response || fetch(event.request);
-
-})
-
-);
-
-
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+        .then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
